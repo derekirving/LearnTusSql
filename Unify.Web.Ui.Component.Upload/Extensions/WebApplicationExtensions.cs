da116f8f61e7c68ca0adddee9d71134f62fd7956
Derek Irving <32242547+derekirving@users.noreply.github.com>
2025-10-22 11:40:48 +0100

    Return WebApplication from MapUnifyUploads extension

--- Unify.Web.Ui.Component.Upload/Extensions/WebApplicationExtensions.cs
+++ Unify.Web.Ui.Component.Upload/Extensions/WebApplicationExtensions.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Builder;
+#if DEBUG
 using Microsoft.Extensions.DependencyInjection;
 using tusdotnet;
 using Unify.Web.Ui.Component.Upload.Interfaces;
+#endif
 
 // ReSharper disable once CheckNamespace
 namespace Unify.Web.Ui.Component.Upload;
 
 public static class WebApplicationExtensions
 {
-    public static void MapUnifyUploads(this WebApplication app)
+    public static WebApplication MapUnifyUploads(this WebApplication app)
     {
         #if DEBUG
         app.UseTus(ctx => ctx.RequestServices.GetRequiredService<ITusConfigurationFactory>().Create(ctx));
         app.MapGet("/upload", () => "Upload endpoint");
         #endif
+        
+        return app;
     }
 }
